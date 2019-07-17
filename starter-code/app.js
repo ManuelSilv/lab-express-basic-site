26708609c16d02526ddeec06cb16010a6fa0afc1
manuelsilv <manuel-silva@live.nl>
2019-07-17 09:48:15 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const app = express ();
+const hbs = require( 'express-handlebars');
+
+// view engine setup
+app.use(express.static('public'))
+app.set('view engine', 'hbs'); // telling express were using hbs
+app.set('views', './views')
+app.engine( 'hbs', hbs( {
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+//routing pages
+app.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+app.get ('/about', (req, res) => {
+  res.render('about' );
+})
+
+app.get ('/contact', (req, res) => {
+  res.render('contact');
+})
+
+
+//localhost listener
+app.listen(3000, () =>{
+   console.log('My first app listening on port 3000!')
+});
